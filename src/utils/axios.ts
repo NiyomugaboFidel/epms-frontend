72217079d5995e8b2319fec49ae28a8ad1d03a0c
NiyomugaboFidel <fidelniyomugabo67@gmail.com>
2026-02-12 00:38:21 +0200

    fix toast error

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,4 @@
 import axios from "axios";
-import { toast } from "sonner";
-
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL || 'https://epms-backend-production.up.railway.app',
     headers:{
